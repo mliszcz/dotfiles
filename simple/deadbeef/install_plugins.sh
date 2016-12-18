8688359b152c1060bc39749e0b75144a73c2af9d
mliszcz <liszcz.michal@gmail.com>
2016-12-18 23:31:34 +0100

    deadbeef plugin install path fixed

--- simple/deadbeef/install_plugins.sh
+++ simple/deadbeef/install_plugins.sh
@@ -5,8 +5,10 @@ DDB_LIBDIR=~/.local/lib/deadbeef
 function ddb_plugin {
   mkdir -p $DDB_LIBDIR
   curl -Lo ddb_plugin.zip $1
-  unzip -d $DDB_LIBDIR ddb_plugin.zip
+  unzip ddb_plugin.zip
   rm -f ddb_plugin.zip
+  mv plugins/* $DDB_LIBDIR/
+  rm -rf plugins
 }
 
 ddb_plugin 'http://sourceforge.net/projects/deadbeef/files/plugins/x86_64/ddb_filebrowser-1562809-x86_64.zip/download'
