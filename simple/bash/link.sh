ac07cb9f55fafcf2f990d116b7c7bc4c471f9751
mliszcz <liszcz.michal@gmail.com>
2016-12-20 00:30:43 +0100

    bash setup added

---
+++ simple/bash/link.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -v
+
+# git completion scripts:
+# Arch:   /usr/share/git/completion
+# Fedora: /usr/share/git-core/contrib/completion
+# Ubuntu: none
+# upstream:
+# https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+# https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -sf $DIR/resources/profile      ~/.profile
+ln -sf $DIR/resources/bash_profile ~/.bash_profile
+ln -sf $DIR/resources/bash_aliases ~/.bash_aliases
+ln -sf $DIR/resources/bashrc       ~/.bashrc
+ln -sf $DIR/resources/bashrc.d     ~/.bashrc.d
+
+set +v
