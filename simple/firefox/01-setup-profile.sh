3909dee3f0e38bb3bb4f6eed961e1983f25c129f
mliszcz <liszcz.michal@gmail.com>
2016-12-22 22:27:46 +0100

    xul setup removed from ff; user.js linked in ff

--- simple/firefox/01-setup-profile.sh
+++ simple/firefox/01-setup-profile.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+# http://kb.mozillazine.org/User.js_file
+
+set -v
+
 PROFILE_DIR=`firefox -CreateProfile default 2>&1 | tail -n 1 | awk '{ print $NF }' | xargs dirname`
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cp $DIR/resources/* $PROFILE_DIR/
+ln -sf $DIR/resources/prefs.js $PROFILE_DIR/user.js
+
+set +v
--- simple/firefox/02-install-addons.sh
+++ simple/firefox/02-install-addons.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 # http://askubuntu.com/questions/73474/how-to-install-firefox-addon-from-command-line-in-scripts
+
+set -v
+
+set +v
