7d43bdda79e7f7718a4835735bd6045e66f8e590
mliszcz <liszcz.michal@gmail.com>
2019-12-28 11:23:44 +0100

    [fish] enable vi key bindings

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,14 @@
+
 set fish_prompt_pwd_dir_length 0
+
+fish_vi_key_bindings
+
+set fish_cursor_default     block      blink
+set fish_cursor_insert      line       blink
+set fish_cursor_replace_one underscore blink
+set fish_cursor_visual      block
+
+fzf_key_bindings
 
 function ll
     command exa --long --classify --all --group --git --sort=name
