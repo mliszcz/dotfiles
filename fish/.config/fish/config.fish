cd5b74f98efb0ba132c2b9a052267d09343a1ce3
mliszcz <liszcz.michal@gmail.com>
2024-06-05 11:27:49 +0200

    [fish] Update fzf keybindings configuration
    There is a new way for configuring keybindings.
    See: https://github.com/junegunn/fzf/issues/3786

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,18 +1,15 @@
-
 set fish_prompt_pwd_dir_length 0
-
-fish_vi_key_bindings
 
 set fish_cursor_default     block      blink
 set fish_cursor_insert      line       blink
 set fish_cursor_replace_one underscore blink
 set fish_cursor_visual      block
 
-fzf_key_bindings
-
 alias ll='eza --long --classify --all --group --sort=name'
 alias tree='eza --tree --long --classify --all --group --sort=name -I=".git|.svn"'
 alias cat='bat'
+
+fish_vi_key_bindings
 
 # CTRL-f completes and executes the autosuggestion.
 bind -s -M insert \ce forward-char
@@ -25,3 +22,7 @@ bind -s -M insert  \cz 'fg 2>/dev/null; commandline -f repaint'
 # Overwrite the default delete-or-exit behavior for CTRL-D.
 bind -s -M default --erase --preset \cd
 bind -s -M insert  --erase --preset \cd
+
+if type -q fzf
+  fzf --fish | source
+end
